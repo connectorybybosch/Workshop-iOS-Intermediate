045c72caf4fef9f2fedcd96c9eabcae563ca111b
Daniel Sánchez <danielreyessanchez@MacBook-Pro-de-Daniel.local>
2018-03-29 08:04:35 -0600

    New Starting Project, with more advances due to few time in Workshop

--- StartingProject/WorkshopiOSVsAndroid/WorkshopiOSVsAndroid/ApiService/ApiService.swift
+++ StartingProject/WorkshopiOSVsAndroid/WorkshopiOSVsAndroid/ApiService/ApiService.swift
@@ -7,13 +7,34 @@
 //
 
 import Foundation
-
+//import Alamofire
 
 class ApiService {
     
     static let sharedInstance = ApiService()
     private let url = "https://thawing-waters-35250.herokuapp.com/workshop"
     
-    
+//    func getXDKData(_ success:@escaping ([Measure]) -> Void,
+//                    _ failure:@escaping (String) -> Void) {
+//        
+//        Alamofire.request(url, method: HTTPMethod.get, parameters: nil, encoding: JSONEncoding.default, headers: nil).validate().responseJSON { (response) in
+//
+//
+//            guard response.result.isSuccess else {
+//                failure(response.result.error?.localizedDescription ?? "Error")
+//                return
+//            }
+//
+//            guard let data = response.result.value else { failure("Error"); return }
+//
+//            do {
+//                let jsonData = try JSONSerialization.data(withJSONObject: data, options: .prettyPrinted)
+//                let measures = try JSONDecoder().decode([Measure].self , from: jsonData)
+//                success(measures)
+//            } catch let error{
+//                failure(error.localizedDescription)
+//            }
+//        }
+//    }
 }
 
---
+++ StartingProject/WorkshopiOSVsAndroid/WorkshopiOSVsAndroid/Protocols/CardRepresentable.swift
@@ -0,0 +1,14 @@
+//
+//  CardRepresentable.swift
+//  WorkshopiOSVsAndroid
+//
+//  Created by Daniel Reyes Sánchez on 29/03/18.
+//  Copyright © 2018 Daniel Reyes Sánchez. All rights reserved.
+//
+
+import UIKit
+
+protocol CardRepresentable {
+    var rowHeight:CGFloat {get}
+    func cellInstance(_ tableView:UITableView, _ indexPath:IndexPath) -> UITableViewCell
+}
